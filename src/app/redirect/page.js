966f84aad6dc45f73678a7ba236ec42e57fe1871
newangry <yevhenii.yevtushenko1@gmail.com>
2023-10-30 12:16:35 -0300

    first commit

---
+++ src/app/redirect/page.js
@@ -0,0 +1,54 @@
+"use client";
+
+import React, { useEffect } from "react";
+import axios from "axios";
+import Image from "next/image";
+import { useSearchParams } from 'next/navigation'
+import { useRouter } from 'next/navigation'
+import { useDispatch } from 'react-redux'
+import { setCredentials } from '../../redux/auth/authSlice'
+
+const Redirect = () => {
+
+    const dispatch = useDispatch()
+    const searchParams = useSearchParams()
+    const router = useRouter()
+    useEffect(() => {
+      const code = searchParams.get('code')
+    if (code) {
+      const serverEndpoint = `http://localhost:8001/users/login/google?code=${code}`;
+      axios
+        .post(serverEndpoint)
+        .then((response) => {
+          if (response.status === 200) {
+              // dispatch(setCredentials({access_token:response.data.access_token}))
+              window.sessionStorage.setItem("access_token", response.data.access_token);
+              router.push("/panel")
+          } else {
+              router.push("/login")
+          }
+        })
+        .catch((error) => {
+            router.push("/login")
+        });
+    } else {
+
+    }
+  }, []);
+
+  return (
+    <div className="flex flex-col justify-center items-center h-screen">
+      <Image
+        src="Confused.svg"
+        alt="login image"
+        width={0}
+        height={0}
+        sizes="100vw"
+        style={{ width: "20%", height: "auto" }}
+      />
+      <h1 className="mt-2 font-extrabold">Loading...</h1>
+    </div>
+  );
+};
+
+export default Redirect;
---
+++ src/hooks/api/api.js
@@ -0,0 +1,124 @@
+'use client'
+import axios from "axios";
+import { toast } from 'react-toastify';
+import {useRouter} from "next/navigation";
+
+const axiosParams = {
+    // baseURL: 'http://64.226.125.111:8000/'
+    baseURL: 'http://localhost:8001/'
+}
+
+const axiosInstance = axios.create(axiosParams);
+
+const api = {
+    get: async (url, config = {}) => {
+        try {
+            const response = await axiosInstance.get(url, {
+                ...config,
+                headers: {
+                    ...config.headers,
+                    'Authorization': `Bearer ${window.sessionStorage.getItem("access_token")}`,
+                }
+            });
+            return response.data
+        } catch (error) {
+            if (error.response.status === 401) {
+                window.location.href = "/login"
+            }else {
+                toast.error("the connection has error!", {
+                    position: toast.POSITION.TOP_CENTER
+                });
+            }
+        }
+    },
+    
+    delete: async (url, config = {}) => {
+        try {
+            const response = await axiosInstance.delete(url, {
+                ...config,
+                headers: {
+                    ...config.headers,
+                    'Authorization': `Bearer ${window.sessionStorage.getItem("access_token")}`,
+                }
+            });
+
+            return response.data
+        } catch (error) {
+            if (error.response.status === 401) {
+                window.location.href = "/login"
+            }else {
+                toast.error("the connection has error!", {
+                    position: toast.POSITION.TOP_CENTER
+                });
+            }
+        }
+    },
+
+    post: async (url, body, config = {}) => {
+        try {
+            const response = await axiosInstance.post(url,body, {
+                ...config,
+                headers: {
+                    ...config.headers,
+                    'Authorization': `Bearer ${window.sessionStorage.getItem("access_token")}`,
+                }
+            });
+
+            return response.data
+        } catch (error) {
+
+            if (error.response.status === 401) {
+                window.location.href = "/login"
+            }else {
+                toast.error("the connection has error!", {
+                    position: toast.POSITION.TOP_CENTER
+                });
+            }
+        }
+    },
+    postFile: async (url, body, config = {}) => {
+        try {
+            const response = await axiosInstance.post(url,body, {
+                ...config,
+                headers: {
+                    ...config.headers,
+                    'Authorization': `Bearer ${window.sessionStorage.getItem("access_token")}`,
+                    'Content-Type': 'multipart/form-data'
+                }
+            });
+            return response.data
+        } catch (error) {
+            if (error.response.status === 401) {
+                window.location.href = "/login"
+            }else {
+                toast.error("the connection has errrrrrrror!", {
+                    position: toast.POSITION.TOP_CENTER
+                });
+            }
+        }
+    },
+    put: async (url, body, config = {}) => {
+        try {
+            const response = await axiosInstance.put(url,body, {
+                ...config,
+                headers: {
+                    ...config.headers,
+                    'Authorization': `Bearer ${window.sessionStorage.getItem("access_token")}`,
+                }
+            });
+
+            return response.data
+        } catch (error) {
+
+            if (error.response.status === 401) {
+                window.location.href = "/login"
+            }else {
+                toast.error("the connection has error!", {
+                    position: toast.POSITION.TOP_CENTER
+                });
+            }
+        }
+    }
+}
+
+export default api;
